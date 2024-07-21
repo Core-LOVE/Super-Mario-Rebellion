414613947f90ff0580ac970bf3b4ca039ac86b29
Core-LOVE <60445133+Core-LOVE@users.noreply.github.com>
2024-07-21 13:27:17 +0400

    water down yay

--- Downpour Sector/luna.lua
+++ Downpour Sector/luna.lua
@@ -4,6 +4,7 @@ clearpipe.registerNPC(36)
 
 local waterY = -199552
 local waterY2 = -120608
+local waterIsDown = nil
 
 local waterfall = {833, 832, 821}
 
@@ -25,4 +26,38 @@ for k,v in ipairs(waterfall) do
 
         fragShader = "waterfall.frag",
     })
+end
+
+local function downWater()
+    local timer = 700
+    local layer = Layer.get("water")
+    local speed = 1
+
+    waterIsDown = speed
+    layer.speedY = speed
+
+    Routine.waitFrames(timer)
+
+    waterIsDown = nil
+    layer.speedY = 0
+end
+
+local waterLayer
+
+function onStart()
+    waterLayer = Section(0).background:get("water")
+end
+
+function onEvent(name)
+    if (name == "switch") then
+        SFX.play("water_drain.ogg", 0.5)
+        Routine.run(downWater)
+    end
+end
+
+function onTick()
+    if not waterIsDown then return end
+
+    waterY = waterY + waterIsDown
+    waterLayer.y = waterLayer.y + waterIsDown
 end
